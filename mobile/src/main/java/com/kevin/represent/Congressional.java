3bf4627696b8945693d51780002ade3ac5d96232
Kevin Jiao <kevin.jiao@berkeley.edu>
2016-03-09 03:03:37 -0800

    Add ArrayAdapter for Representative JSON objects

--- mobile/src/main/java/com/kevin/represent/Congressional.java
+++ mobile/src/main/java/com/kevin/represent/Congressional.java
@@ -11,15 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.internal.IMapFragmentDelegate;
+import com.google.android.gms.plus.internal.model.people.PersonEntity;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.*;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
@@ -31,8 +35,8 @@ import cz.msebera.android.httpclient.Header;
 
 public class Congressional extends AppCompatActivity {
     private ViewGroup mRelativeLayout;
-    ArrayList<String> reps = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+    ArrayList<JSONObject> reps = new ArrayList<JSONObject>();
+    ArrayAdapter<JSONObject> adapter;
 
 
     @Override
@@ -51,8 +55,7 @@ public class Congressional extends AppCompatActivity {
                     JSONArray repObjects = res.getJSONArray("results");
                     for (int i = 0; i < res.getInt("count"); i++) {
                         JSONObject repObj = repObjects.getJSONObject(i);
-                        String name = repObj.getString("first_name") + " " + repObj.getString("last_name");
-                        reps.add(name);
+                        reps.add(repObj);
                     }
                     System.out.println(reps);
                     adapter.notifyDataSetChanged();
@@ -77,11 +80,11 @@ public class Congressional extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private class repAdapter extends ArrayAdapter<String> {
+    private class repAdapter extends ArrayAdapter<JSONObject> {
         Context context;
-        List<String> objects;
+        List<JSONObject> objects;
 
-        public repAdapter(Context context, int resource, List<String> objects) {
+        public repAdapter(Context context, int resource, List<JSONObject> objects) {
             super(context, resource, objects);
             this.context = context;
             this.objects = objects;
@@ -89,12 +92,33 @@ public class Congressional extends AppCompatActivity {
 
         @Override
         public View getView(int pos, View convertView, ViewGroup parent) {
+            JSONObject rep = objects.get(pos);
             LayoutInflater inflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View repView = inflater.inflate(R.layout.rep_card, parent, false);
             TextView name = (TextView) repView.findViewById(R.id.name);
-            name.setText(objects.get(pos));
-            return repView;
+            TextView desc = (TextView) repView.findViewById(R.id.desc);
+            ImageView home = (ImageView) repView.findViewById(R.id.home);
+            ImageView email = (ImageView) repView.findViewById(R.id.email);
+            ImageView twitter = (ImageView) repView.findViewById(R.id.twitter);
+            ImageView info = (ImageView) repView.findViewById(R.id.info);
+
+            try {
+                StringBuilder sb = new StringBuilder();
+                sb.append(rep.getString("title"))
+                        .append(" ")
+                        .append(rep.getString("first_name"))
+                        .append(" ")
+                        .append(rep.getString("last_name"))
+                        .append(" ")
+                        .append(rep.getString("party"));
+                name.setText(sb.toString());
+                desc.setText(rep.getString("website"));
+                return repView;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
     }
 
