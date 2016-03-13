51b65cfbad70b769aff1577c9538de2bfe1ff9d7
Kevin Jiao <kevin.jiao@berkeley.edu>
2016-03-12 18:05:28 -0800

    Add shake to randomize location

--- wear/src/main/java/com/kevin/represent/Vote.java
+++ wear/src/main/java/com/kevin/represent/Vote.java
@@ -9,8 +9,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONObject;
+
+import java.util.Random;
 
 public class Vote extends Activity implements SensorEventListener {
     SensorManager sensorMgr;
@@ -18,11 +21,45 @@ public class Vote extends Activity implements SensorEventListener {
     float xaccel;
     float yaccel;
     float zaccel;
+    LinearLayout romney;
+    LinearLayout obama;
+    TextView rText;
+    TextView oText;
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (xaccel == 0.0f) {
+            xaccel = event.values[0];
+            yaccel = event.values[1];
+            zaccel = event.values[2];
+            return;
+        }
 
+        if ((xaccel != event.values[0] || yaccel != event.values[1] || zaccel != event.values[2])) {
+            Random rand = new Random();
+            int lat = rand.nextInt(148) + -124;
+            int lon = rand.nextInt(116) + -66;
+            xaccel = event.values[0];
+            yaccel = event.values[1];
+            zaccel = event.values[2];
+
+            float Rvote = ((int) (rand.nextFloat() * 1000)) / 10;
+            float Ovote = 100 - Rvote;
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(romney.getLayoutParams());
+            lp.weight = Rvote;
+            romney.setLayoutParams(lp);
+
+            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(obama.getLayoutParams());
+            lp2.weight = Ovote;
+            obama.setLayoutParams(lp2);
+            String s = "Mitt Romney: " + Rvote + "%";
+            rText.setText(s);
+            String s2 = "Barack Obama: " + Ovote + "%";
+            oText.setText(s2);
+            Toast.makeText(this, "Showing results for location " + lat + ", " + lon,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -35,10 +72,10 @@ public class Vote extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vote);
         Intent intent = getIntent();
-        LinearLayout romney = (LinearLayout) findViewById(R.id.romney);
-        LinearLayout obama = (LinearLayout) findViewById(R.id.obama);
-        TextView rText = (TextView) findViewById(R.id.rtext);
-        TextView oText = (TextView) findViewById(R.id.otext);
+        romney = (LinearLayout) findViewById(R.id.romney);
+        obama = (LinearLayout) findViewById(R.id.obama);
+        rText = (TextView) findViewById(R.id.rtext);
+        oText = (TextView) findViewById(R.id.otext);
         double Rvote, Ovote;
         try {
             JSONObject result = new JSONObject(intent.getStringExtra("results"));
